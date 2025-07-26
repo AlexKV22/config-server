6f882cc73ac8b394acaf1ef8bdb64a1e2cd09a10
aleksandrkakovkin <aleksandrkakovkin44@gmail.com>
2025-07-26 17:10:32 +0300

    Add test method

--- src/test/java/org/example/configserver/ConfigServerApplicationTests.java
+++ src/test/java/org/example/configserver/ConfigServerApplicationTests.java
@@ -1,13 +1,28 @@
 package org.example.configserver;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.properties")
 class ConfigServerApplicationTests {
+
+    private final TestRestTemplate testRestTemplate;
+
+    @Autowired
+    public ConfigServerApplicationTests(TestRestTemplate testRestTemplate) {
+        this.testRestTemplate = testRestTemplate;
+    }
 
     @Test
     void contextLoads() {
+        ResponseEntity<String> response = testRestTemplate.getForEntity("/gateway/default", String.class);
+        Assertions.assertTrue(response.getStatusCode().is2xxSuccessful());
+        Assertions.assertTrue(response.getBody().contains("8761"));
     }
-
 }
