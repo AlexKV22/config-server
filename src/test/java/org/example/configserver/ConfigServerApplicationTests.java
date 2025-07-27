de6ca629c3cfd80f1f40041e09b969cf1dd7db1e
aleksandrkakovkin <aleksandrkakovkin44@gmail.com>
2025-07-27 13:20:50 +0300

    Refactor

--- src/test/java/org/example/configserver/ConfigServerApplicationTests.java
+++ src/test/java/org/example/configserver/ConfigServerApplicationTests.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource("classpath:application-test.properties")
 class ConfigServerApplicationTests {
 
     private final TestRestTemplate testRestTemplate;
